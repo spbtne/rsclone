472cdb92b8b76b460bd7fda0d02e16760531c596
Voloshin_Sergey <4voloshin@gmail.com>
2021-01-27 01:29:15 +0300

    feat: add audio bubble layout

--- chat/src/components/Message/indexMessage.js
+++ chat/src/components/Message/indexMessage.js
@@ -1,17 +1,20 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 // import formatDistanceToNow from "date-fns/formatDistanceToNow";
 // import ruLocale from "date-fns/locale/ru";
-import classNames from "classnames";
-import readedSvg from "../../assets/img/readed.svg";
-import noReadedSvg from "../../assets/img/no-read.svg";
-import readedSvgWhite from "../../assets/img/readedWhite.svg";
-import noReadedSvgWhite from "../../assets/img/no-readWhite.svg";
+import classNames from 'classnames';
+import readedSvg from '../../assets/img/readed.svg';
+import noReadedSvg from '../../assets/img/no-read.svg';
+import readedSvgWhite from '../../assets/img/readedWhite.svg';
+import noReadedSvgWhite from '../../assets/img/no-readWhite.svg';
+import waveSvg from '../../assets/img/wave.svg';
+import playSvg from '../../assets/img/play.svg';
+import pauseSvg from '../../assets/img/pause.svg';
 
-import Time from '../Time/indexTime'
+import Time from '../Time/indexTime';
 
-import "./Message.scss";
-import IconReaded from "../IconReaded/indexIconReaded";
+import './Message.scss';
+import IconReaded from '../IconReaded/indexIconReaded';
 
 // const Message = ({ avatar, user, text, date, audio, isMe, isReaded, attachments, isTyping }) => {
 //   return (
@@ -59,46 +62,82 @@ import IconReaded from "../IconReaded/indexIconReaded";
 //   );
 // }
 
-function Message ({ avatar, user, text, date, isMe, isReaded, attachments, isTyping }) {
+function Message({ avatar, user, text, date, audio, isMe, isReaded, attachments, isTyping }) {
+  const [isPlaying, setIsPlaing] = useState(false);
+
   return (
-    <div className={classNames("message", {
-    "message--isme": isMe,
-    "message--is-typing": isTyping,
-    "message--image": attachments && attachments.length === 1 })}>
+    <div
+      className={classNames('message', {
+        'message--isme': isMe,
+        'message--is-typing': isTyping,
+        'message--is-audio': audio,
+        'message--image': attachments && attachments.length === 1,
+      })}>
       <div className="message__avatar">
         <img src={avatar} alt={`${user.fullname} avatar`} />
       </div>
       <div className="message__container">
-        <IconReaded isMe={isMe} isReaded={isReaded}/>
-        {(text || isTyping) && <div className="message__bubble">
-          {text && <p className="message__text">{text}</p>}
-            {isTyping && <div className="message__typing">
-            <span />
-            <span />
-            <span />
-          </div>}
-          <div class="chat-bubble">
-
+        <IconReaded isMe={isMe} isReaded={isReaded} />
+        {(audio || text || isTyping) && (
+          <div className="message__bubble">
+            {text && <p className="message__text">{text}</p>}
+            {isTyping && (
+              <div className="message__typing">
+                <span />
+                <span />
+                <span />
+              </div>
+            )}
+            {audio && (
+              <div className="message__audio">
+                <div className="message__audio-progress" style={{ width: '40%' }}></div>
+                <div className="message__audio-info">
+                  <div className="message__audio-btn">
+                    <button>
+                      {isPlaying ? (
+                        <img src={pauseSvg} alt="Pause svg" />
+                      ) : (
+                        <img src={playSvg} alt="Play svg" />
+                      )}
+                    </button>
+                  </div>
+                  <div className="message__audio-wave">
+                    <img src={waveSvg} alt="Wave svg" />
+                  </div>
+                  <span className="message__audio-duration">00:19</span>
+                </div>
+              </div>
+            )}
+            <div class="chat-bubble"></div>
+            <img
+              src={
+                isMe
+                  ? isReaded
+                    ? readedSvg
+                    : noReadedSvg
+                  : isReaded
+                  ? readedSvgWhite
+                  : noReadedSvgWhite
+              }
+              alt="Check icon"
+              className="message__check-icon"
+            />
           </div>
-          <img
-            src={isMe ? (isReaded ? readedSvg : noReadedSvg) : (isReaded ? readedSvgWhite : noReadedSvgWhite)}
-            alt="Check icon"
-            className="message__check-icon"
-          />
-        </div>}
-        <div className="message__attachments">
-          {attachments &&
-            attachments.map((item) => (
+        )}
+        {attachments && (
+          <div className="message__attachments">
+            {attachments.map((item) => (
               <div className="message__attachments-item">
                 <img src={item.url} alt={item.filename} />
               </div>
             ))}
-        </div>
+          </div>
+        )}
         {date && (
-        <span className="message__date">
-          <Time date={date} />
-        </span>
-  )}
+          <span className="message__date">
+            <Time date={date} />
+          </span>
+        )}
       </div>
     </div>
   );
@@ -117,7 +156,7 @@ Message.propTypes = {
   isMe: PropTypes.bool,
   isReaded: PropTypes.bool,
   isTyping: PropTypes.bool,
-  audio: PropTypes.string
+  audio: PropTypes.string,
 };
 
 export default Message;
