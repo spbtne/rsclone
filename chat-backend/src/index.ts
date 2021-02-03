8c3fe9885f8e89747c751e471e19a890d9efbfe0
Nikita Tushin <spbtne@gmail.com>
2021-02-03 21:57:08 +0300

    add: mongo atlas

--- chat-backend/src/index.ts
+++ chat-backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 
-import './core/db';
+
 import createRoutes from './core/routes';
 import createSocket from './core/socket';
 
@@ -13,7 +13,39 @@ dotenv.config();
 
 createRoutes(app, io);
 
+import mongoose from "mongoose";
 
-server.listen(process.env.PORT, function () {
-  console.log(`Server: http://localhost:${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 3000
+
+// mongoose.connect("mongodb://localhost:27017/chat", {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false
+// });
+
+async function start() {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://nikita:12345@cluster0.iu3mn.mongodb.net/chat',
+      {
+        
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false
+        
+      }
+    )
+    server.listen(process.env.PORT, function () {
+      console.log(`Server works`);
+    })
+    
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
+
